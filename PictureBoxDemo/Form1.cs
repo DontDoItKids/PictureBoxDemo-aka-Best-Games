9b98c22fb986b647c00a66b457a1f9d6a66b52fe
Trey Hall <Trey.Hall@mymhc.ca>
2022-09-14 09:37:45 -0600

    Made so only one game shows at a time.

--- PictureBoxDemo/Form1.cs
+++ PictureBoxDemo/Form1.cs
@@ -15,16 +15,22 @@ namespace PictureBoxDemo
         private void btn1st_Click(object sender, EventArgs e)
         {
             picBestGame.Visible = true;
+            picSecondBest.Visible = false;
+            picThirdBest.Visible = false;
         }
 
         private void btn2nd_Click(object sender, EventArgs e)
         {
             picSecondBest.Visible = true;
+            picBestGame.Visible = false;
+            picThirdBest.Visible = false;
         }
 
         private void btn3rd_Click(object sender, EventArgs e)
         {
             picThirdBest.Visible = true;
+            picBestGame.Visible = false;
+            picSecondBest.Visible = false;
         }
     }
 }
